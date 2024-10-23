b6614c31aae8918ae0a4cf866e9126474b650b68
Rafael Arcanjo <rafael.arcanjo.dssilva@gmail.com>
2024-10-22 23:03:07 -0300

    Ex sobre o MATH

---
+++ aula15/index.js
@@ -0,0 +1,8 @@
+let num1 = 9.8967846
+
+console.log(Math.floor(num1))
+console.log(Math.ceil(num1))
+console.log(Math.round(num1))
+console.log(Math.max(1, 3, 5, 2, 23, 53532, 4, -2, 53, -50, -73))
+console.log(Math.min(1, 3, 5, 2, 23, 53532, 4, -2, 53, -50, -73))
+console.log(Math.round(Math.random() * (10-5) + 5))
