a5c563f27c481a1dd599e7af0c12ed400c6e31de
Rafael Arcanjo <rafael.arcanjo.dssilva@gmail.com>
2024-10-27 00:23:22 -0300

    Estudo sobre funções

---
+++ aula17/index.js
@@ -0,0 +1,12 @@
+function somar(x = 1, y = 1) {
+    const res = x + y
+    return res
+};
+
+console.log(`O resultado é ${somar(8, 8)}`)
+
+const raiz = function (n) {
+    return n ** 0.5;
+};
+
+console.log(raiz(16))
