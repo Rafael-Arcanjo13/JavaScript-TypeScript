9512d22f0ebbe625ec47f4822ef9e6a12203973c
Rafael Arcanjo <rafael.arcanjo.dssilva@gmail.com>
2024-11-12 23:06:04 -0300

    Var e let

---
+++ aula29/index.js
@@ -0,0 +1,16 @@
+const verdadeira = true;
+
+let nome = 'Luiz'
+var nome2 = 'Luiz'
+
+if (verdadeira) {
+    let nome = 'Otavio'
+    console.log(nome)
+
+    if (verdadeira) {
+        let nome = 'Outra coisa'
+        console.log(nome)
+    }
+}
+
+//Algumas diferenças entre var e let
