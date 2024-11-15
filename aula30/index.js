f560707829710a37e416e2d0b755f979b8058258
Rafael Arcanjo <rafael.arcanjo.dssilva@gmail.com>
2024-11-14 23:52:12 -0300

    Atribuição via desestruturação (Arrays)

--- aula30/index.js
+++ aula30/index.js
@@ -11,6 +11,12 @@ console.log(a, b, c)*/
 //const primeiroNumero = numeros[0]
 //console.log(primeiroNumero)
 
-const numeros = [16, 24, 32, 47, 54, 65, 75, 836, 93]
-let [num1, num2, num3, ...resto] = numeros
-console.log(resto)
+//const numeros = [16, 24, 32, 47, 54, 65, 75, 836, 93]
+//let [num1, num2, num3, ...resto] = numeros
+//console.log(resto)
+
+const numeros = [ [1, 2, 3], [4, 5, 6], [7, 8, 9] ];
+//const [, [, , seis]] = numeros;
+//console.log(seis);
+const [lista1, lista2, lista3] = numeros
+console.log(lista3[2])
