7f05beb06edf9284b8b502fbf131d8de0e1616ec
Rafael Arcanjo <rafael.arcanjo.dssilva@gmail.com>
2024-11-18 23:26:10 -0300

    Estudando sobre o for in no array e no object

---
+++ aula36/index.js
@@ -0,0 +1,21 @@
+const pessoa = {
+    nome: 'Rafael',
+    sobrenome: 'Arcanjo',
+    idadde: 18
+};
+
+console.log(pessoa.nome)
+console.log(pessoa['nome'])
+
+//for (let chaves in pessoa) {
+//    console.log(chaves)
+//}
+
+//for (let indice in frutas) {
+//    console.log(frutas[indice])
+//}
+
+//for (let i = 0; i < frutas.length; i++) {
+//    console.log(frutas[i])
+//}
+
