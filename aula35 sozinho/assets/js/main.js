e8c660eb57e87d8e8fec9b816b8516a19eb64a80
Rafael Arcanjo <rafael.arcanjo.dssilva@gmail.com>
2024-11-18 22:19:33 -0300

    Array de objetos declarado

--- aula35 sozinho/assets/js/main.js
+++ aula35 sozinho/assets/js/main.js
@@ -5,16 +5,3 @@ const elementos = [
     {tag: 'footer', texto: 'Frase 4'},
 ];
 
-const container = document.querySelector('.container');
-const div = document.createElement('div');
-
-for (let i = 0; i < elementos.length; i++) {
-    let { tag, texto } = elementos[i];
-    let tagCriada = document.createElement(tag);
-    let textoCriado = document.createTextNode(texto)
-
-    tagCriada.appendChild(textoCriado)
-    div.appendChild(tagCriada)
-}
-
-container.appendChild(div)
