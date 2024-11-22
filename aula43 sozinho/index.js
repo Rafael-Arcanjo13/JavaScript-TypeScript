7624c70a96035ac7e34244efec38ab190aa7c8c5
Rafael Arcanjo <rafael.arcanjo.dssilva@gmail.com>
2024-11-21 23:22:33 -0300

    Ajustei kkk

--- aula43 sozinho/index.js
+++ aula43 sozinho/index.js
@@ -1,18 +1,22 @@
 function fizzBuzz(num) {
     if (isNaN(num)) {
-        return console.log(num)
+        return num
     }
 
     if (num % 3 === 0 && num % 5 === 0) {
-        return console.log('FizzBuzz')
+        return 'FizzBuzz';
     } else if (num % 3 === 0) {
-        return console.log('Fizz')
+        return 'Fizz';
     } else if (num % 5 === 0) {
-        return console.log('Buzz')
+        return 'Buzz';
     } else if (num % 3 !== 0 && num % 5 !== 0) {
-        return console.log(num)
+        return num;
     }
 }
 
-num = Math.random() * (0, 100) + 0
-fizzBuzz(Math.floor(num))
+//num = Math.random() * (0, 100) + 0
+//fizzBuzz(Math.floor(num))
+
+for (let i = 0; i <= 100; i++) {
+    console.log(i, fizzBuzz(i))
+}
