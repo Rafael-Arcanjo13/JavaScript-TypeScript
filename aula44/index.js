c320393c5e28817bf2c19fb39cd44a59a65d4d3a
Rafael Arcanjo <rafael.arcanjo.dssilva@gmail.com>
2024-11-22 23:20:47 -0300

    try, catch e throw

--- aula44/index.js
+++ aula44/index.js
@@ -1,5 +1,20 @@
-try {
-    console.log(naoExisto)
+/*try {
+    console.log(numero1)
 } catch (err) {
-    console.log('naoExisto não existe')
+    console.log('numero1 não existe!')
+}*/
+
+function soma(x, y) {
+    if (typeof x !== 'number' || typeof y !== 'number') {
+        throw new Error('x e y precisam ser numeros.');
+    }
+
+    return x + y
+}
+
+try {
+    console.log(soma(1, 2))
+    console.log(soma('1', 2))
+} catch(err) {
+    console.log('Por favor, digite um número!')
 }
