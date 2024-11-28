71e086aa299d16e960677fbd4df51dfb17981ef8
Rafael Arcanjo <rafael.arcanjo.dssilva@gmail.com>
2024-11-27 23:38:15 -0300

    Utilizando o .target para minimizar a quantidade de eventos e funções. Envolvendo o codigo inteiro em uma função. Finalizado!

--- aula47 videotutorial/assets/js/main.js
+++ aula47 videotutorial/assets/js/main.js
@@ -1,39 +1,46 @@
-function criaHoraDosSegundos(segundos) {
-    const data = new Date(segundos * 1000);
-    return data.toLocaleTimeString('pt-BR', {
-        hour12: false,
-        timeZone: 'UTC'
+function relogio() {
+    function criaHoraDosSegundos(segundos) {
+        const data = new Date(segundos * 1000);
+        return data.toLocaleTimeString('pt-BR', {
+            hour12: false,
+            timeZone: 'UTC'
+        })
+    }
+
+    const relogio = document.querySelector('.relogio');
+    const iniciar = document.querySelector('.iniciar');
+    const pausar = document.querySelector('.pausar');
+    const zerar = document.querySelector('.zerar');
+
+    let segundos = 0;
+    let timer;
+
+    function iniciaRelogio() {
+        timer = setInterval(function () {
+            segundos++
+            relogio.innerHTML = criaHoraDosSegundos(segundos);
+        }, 1000)
+    }
+
+    document.addEventListener('click', function (event) {
+        const elementoClicado = event.target;
+
+        if (elementoClicado.classList.contains('iniciar')) {
+            clearInterval(timer);
+            iniciaRelogio();
+            relogio.classList.remove('pausado');
+        }
+
+        if (elementoClicado.classList.contains('pausar')) {
+            clearInterval(timer);
+            relogio.classList.add('pausado');
+        }
+
+        if (elementoClicado.classList.contains('zerar')) {
+            clearInterval(timer);
+            relogio.classList.remove('pausado');
+            relogio.innerHTML = '00:00:00'
+        }
     })
 }
-
-const relogio = document.querySelector('.relogio');
-const iniciar = document.querySelector('.iniciar');
-const pausar = document.querySelector('.pausar');
-const zerar = document.querySelector('.zerar');
-
-let segundos = 0;
-let timer;
-
-function iniciaRelogio() {
-    timer = setInterval(function() {
-        segundos++
-        relogio.innerHTML = criaHoraDosSegundos(segundos);
-    }, 1000)
-}
-
-iniciar.addEventListener('click', function(event) {
-    relogio.classList.remove('pausado')
-    clearInterval(timer)
-    iniciaRelogio();
-});
-
-pausar.addEventListener('click', function(event) {
-    relogio.classList.add('pausado')
-    clearInterval(timer)
-});
-
-zerar.addEventListener('click', function(event) {
-    clearInterval(timer)
-    relogio.innerHTML = '00:00:00'
-    segundos = 0
-});
+relogio();
