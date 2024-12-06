d3bbd06996e508a8f31ba522af39cd4b5d8d6aa7
Rafael Arcanjo <rafael.arcanjo.dssilva@gmail.com>
2024-12-05 23:08:14 -0300

    return de function

--- aula51/index.js
+++ aula51/index.js
@@ -2,6 +2,41 @@
 // retorna um valor
 // Termina a função
 
-function soma(a, b) {
+/*function soma(a, b) {
     return a + b;
 }
+
+function soma2(a, b) {
+    console.log(a + b)
+}
+
+soma2(2, 3)*/
+
+/*document.addEventListener('click', function() {
+    document.body.style.backgroundColor = 'red';
+})*/ //essa function não retorna valor
+
+/*function falaComeco(comeco) {
+    function falaResto(resto) {
+        return comeco + ' ' + resto
+    }
+    return falaResto
+}
+
+const primeiraFala = falaComeco('Olá')
+const falaCompleta = primeiraFala('Mundo!')
+console.log(falaCompleta)*/ //*function com retorno aninhadas*
+
+function criaMultiplicador(multiplicador) {
+    return function(numero) {
+        return numero * multiplicador
+    }
+}
+
+const duplica = criaMultiplicador(2)
+const triplica = criaMultiplicador(3)
+const quadriplica = criaMultiplicador(4)
+
+console.log(duplica(2))
+console.log(triplica(2))
+console.log(quadriplica(2))
