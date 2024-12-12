6b5f59a4677356d913adb42e7eb9284ab376a368
Rafael Arcanjo <rafael.arcanjo.dssilva@gmail.com>
2024-12-12 00:02:57 -0300

    Concluída!

--- aula57/assets/js/main.js
+++ aula57/assets/js/main.js
@@ -1,14 +1,76 @@
-function meuEscopo() {
-    const display = document.querySelector('.display')
-    const btnEq = document.querySelector('.btn-eq')
-    const numeros = document.querySelectorAll('.btn-num')
+function criaCalculadora() {
+  return {
+    display: document.querySelector('.display'),
+    
+    inicia() {
+      this.cliqueBotoes();
+      this.pressionaEnter();
+    },
 
-    numeros.forEach((btn) =>
-        btn.addEventListener('click', (event) => {
-          display.innerHTML = event.currentTarget.textContent
-        })
-      );
+    pressionaEnter() {
+      this.display.addEventListener('keyup', (e) => {
+        if(e.keyCode === 13) {
+          this.realizaConta();
+        }
+      })
+    },
 
+    realizaConta() {
+      let conta = this.display.value;
+
+      try {
+        conta = eval(conta);
+
+        if(!conta) {
+          alert('Conta inválida!')
+          return;
+        }
+
+        this.display.value = String(conta)
+      } catch (e) {
+        alert('Conta inválida!')
+        return;
+      }
+    },
+
+    clearDisplay() {
+      this.display.value = ''
+    },
+
+    apagaUm() {
+      this.display.value = this.display.value.slice(0, -1)
+    },
+
+
+    cliqueBotoes() {
+      document.addEventListener('click', (e) => { //Utilizar uma array function faz com que o valor não mude, ai não precisa utilizar o .bind(this)
+        const el = e.target
+
+        if(el.classList.contains('btn-num')) {
+          this.btnParaDisplay(el.innerText);
+        }
+
+        if(el.classList.contains('btn-clear')) {
+          this.clearDisplay();
+        }
+
+        if(el.classList.contains('btn-del')) {
+          this.apagaUm();
+        }
+
+        if(el.classList.contains('btn-eq')) {
+          this.realizaConta();
+        }
+
+        this.display.focus();
+      }) //O .bind(this) faz o this continuar sendo o valor inicial que declarou, no caso, a criaCalculadora. 
+    },
+
+    btnParaDisplay(valor) {
+      this.display.value += valor
+    },
+  }
 }
 
-meuEscopo()
+const calculadora = criaCalculadora();
+calculadora.inicia();
