2f2ff29afad1ede00d379a66bf7932086dc348dc
Rafael Arcanjo <rafael.arcanjo.dssilva@gmail.com>
2024-12-21 00:19:37 -0300

    Quase pronta

--- aula59/assets/js/main.js
+++ aula59/assets/js/main.js
@@ -1,22 +1,36 @@
 function Calculadora() {
   this.display = document.querySelector('.display')
 
-  this.btnClicado = () => {
-    document.addEventListener('click', (e) => {
-      this.el = e.target
+  this.btnClicado = document.addEventListener('click', (e) => {
+    this.el = e.target
 
-      if(this.el.classList.contains('btn-num'))
-        this.colocarNoDisplay(el)
-      }
-  
-    )}
+    if(this.el.classList.contains('btn-num')) {
+      this.addBtnClicado(this.el.innerText)
+    }
 
-  } 
+    if(this.el.classList.contains('btn-clear')) {
+      this.display.value = ''
+    }
+
+    if(this.el.classList.contains('btn-del')) {
+      this.display.value = this.display.value.slice(0, -1)
+    }
+
+    if(this.el.classList.contains('btn-eq')) {
+      this.realizaConta()
+    }
+  })
+
+  this.realizaConta = () => {
+    let conta = this.display.value
+
+    conta = eval(conta)
+    this.display.value = String(conta)
+  }
   
-  this.colocarNoDisplay = (valor) => {
-    this.display.value += valor
+  this.addBtnClicado = (valor) => {
+    this.display.value += valor;
   }
 }
 
-const calculator = new Calculadora()
-console.log(calculator)
+const c1 = new Calculadora()
