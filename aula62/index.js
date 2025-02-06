054078852a147f0493bbefbf56054a84b375d28b
Rafael Arcanjo <rafael.arcanjo.dssilva@gmail.com>
2025-02-05 23:40:55 -0300

    Revisão de Arrays

---
+++ aula62/index.js
@@ -0,0 +1,23 @@
+// ============== VALORES POR REFERÊNCIA ==============
+
+//const nomes = ['Rafael', 'Gabriela', 'Alice']
+//const novo = [...nomes]
+
+//novo.pop()
+//console.log(nomes)
+//console.log(novo)
+
+// ============== ARRAY ==============
+
+// const nomes = ['Rafael', 'Gabriela', 'Alice', 'Arcanjo', 'Goldofredo']
+//const novo = nomes.slice(1, 3)
+
+//console.log(novo)
+
+// ============== ARRAY ==============
+
+//const nome = 'Rafael Arcanjo Santos Silva';
+//const nomes = nome.split(' ')
+const nomes = ['Rafael', 'Arcanjo', 'Santos', 'Silva']
+const nome = nomes.join(' ')
+console.log(nome)
