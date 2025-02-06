b044cbd04f946b1615af9d09c3291752cdac60a8
Rafael Arcanjo <rafael.arcanjo.dssilva@gmail.com>
2025-02-05 23:59:19 -0300

    Método SPLICE()

---
+++ aula63/index.js
@@ -0,0 +1,19 @@
+const nomes = ['Rafael', 'Gabriela', 'Gabriel', 'Leticia', 'Gato'];
+
+console.log(nomes)
+
+// UNSHIFT
+//nomes.splice(0, 0, 'Flavia')
+
+// PUSH 
+//nomes.splice(nomes.length, 0, 'Flavia')
+
+// SHIFT
+//const removidos = nomes.splice(0, 1)
+
+// POP
+//const removidos = nomes.splice(-1, 1)
+
+//const removidos = nomes.splice(3,  1, 'Pudim', 'Batata')
+
+//.splice(indice, delete, elem1, elem2, elem3)
