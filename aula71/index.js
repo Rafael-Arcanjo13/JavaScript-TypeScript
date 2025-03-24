c3a9b025b3c8f9a31bdea864f9377f02491889ca
Rafael Arcanjo <rafael.arcanjo.dssilva@gmail.com>
2025-03-24 10:01:28 -0300

    Aula sobre DefineProperty e DefineProperties

--- aula71/index.js
+++ aula71/index.js
@@ -1,12 +1,31 @@
 function Produto(nome, preco, estoque) {
-    this.nome = nome;
-    this.preco = preco;
-
     Object.defineProperty(this, 'estoque', {
-        enumerable: true,
-        value: estoque,
+        enumerable: true, // mostra a chave
+        value: estoque, // valor
+        writable: true, // Pode alterar
+        configurable: true, // Configurável
+    })
+
+    Object.defineProperties(this, {
+        nome: {
+            enumerable: true, // mostra a chave
+            value: nome, // valor
+            writable: true, // Pode alterar
+            configurable: true, // Configurável
+        },
+
+        preco: {
+            enumerable: true, // mostra a chave
+            value: preco, // valor
+            writable: true, // Pode alterar
+            configurable: true, // Configurável
+        },
     })
 }
 
 const p1 = new Produto('Camiseta', 20, 3)
 console.log(p1)
+
+for(let chave in p1) {
+    console.log(chave)
+}
