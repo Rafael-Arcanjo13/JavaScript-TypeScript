03b843a252a6b0830112904438e86af978aed018
Rafael Arcanjo <rafael.arcanjo.dssilva@gmail.com>
2025-04-08 20:45:53 -0300

    Aula sobre herança completa

--- aula76/index.js
+++ aula76/index.js
@@ -12,10 +12,43 @@ Produto.prototype.desconto = function(quantia) {
 
 function Camiseta(nome, preco, cor) {
     Produto.call(this, nome, preco);
+    this.cor = cor;
 }
 
 Camiseta.prototype = Object.create(Produto.prototype);
+Camiseta.prototype.constructor = Camiseta;
 
-const c1 = new Camiseta('Regata', 7.5, 'Preta');
-c1.aumento(10)
-console.log(c1)
+Camiseta.prototype.aumento = function(percentual) {
+    this.preco = this.preco + (this.preco * percentual / 100);
+}
+
+function Caneca(nome, preco, material, estoque) {
+    Produto.call(this, nome, preco);
+    this.material = material;
+
+    Object.defineProperty(this, estoque, {
+        enumerable: true,
+        configurable: false,
+        get: function() {
+            return estoque;
+        },
+        set: function(valor) {
+            if(typeof valor !== 'number') return;
+            estoque = valor
+        }
+
+    })
+}
+
+Caneca.prototype = Object.create(Produto.prototype);
+Caneca.prototype.constructor = Caneca;
+
+const produto = new Produto('Gen', 111);
+const camiseta = new Camiseta('Regata', 7.5, 'Preta');
+const caneca = new Caneca('Caneca', 13, 'Plástico', 5);
+caneca.estoque = 100;
+
+console.log(caneca.estoque)
+console.log(caneca)
+console.log(camiseta)
+console.log(produto)
