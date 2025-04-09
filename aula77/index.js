77e1a81d2040c87d66d2862e0a59855b0ee17f35
Rafael Arcanjo <rafael.arcanjo.dssilva@gmail.com>
2025-04-08 23:41:42 -0300

    Começo da validação de CPF

--- aula77/index.js
+++ aula77/index.js
@@ -1,5 +1,16 @@
-const cpf = '705.484.450-52'
-const cpfLimpo = cpf.replace(/\D+/g, '');
-const cpfArray = Array.from(cpfLimpo);
+function ValidaCpf(cpfEnviado) {
+    Object.defineProperty(this, 'cpfLimpo', {
+        get: function() {
+            return cpfEnviado.replace(/\D+/g, '')
+        }
+    })
+}
 
-console.log(cpfArray)
+ValidaCpf.prototype.valida = function() {
+    if(typeof this.cpfLimpo === 'undefined') return false;
+    if(this.cpfLimpo.length !== 11) return false;
+    return true;
+};
+
+const cpf = new ValidaCpf('705.484.450-52');
+console.log(cpf.valida())
