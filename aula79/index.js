181fc6471e0e8ad4d20dc868391b68ebc585e4e8
Rafael Arcanjo <rafael.arcanjo.dssilva@gmail.com>
2025-04-29 20:39:09 -0300

    Criação de Classes

---
+++ aula79/index.js
@@ -0,0 +1,32 @@
+class Pessoa {
+    constructor(nome, sobrenome) {
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+    }
+
+    falar() {
+        console.log(`${this.nome} está falando.`)
+    }
+
+    comer() {
+        console.log(`${this.nome} está comendo.`)
+    }
+
+    beber() {
+        console.log(`${this.nome} está bebendo.`)
+    }
+}
+
+function Pessoa2(nome, sobrenome) {
+    this.nome = nome;
+    this.sobrenome = sobrenome;
+}
+
+Pessoa2.prototype.falar = function() {
+    console.log(`${this.nome} está falando.`)
+}
+
+const p1 = new Pessoa('Rafael', 'Arcanjo');
+const p2 = new Pessoa2('Rafael', 'Arcanjo');
+p1.falar();
+p2.falar();
