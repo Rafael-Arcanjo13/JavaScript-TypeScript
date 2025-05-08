3e572c55ef8dfae60652acf84fa93719c31bbda2
Rafael Arcanjo <rafael.arcanjo.dssilva@gmail.com>
2025-05-07 23:10:38 -0300

    Nomeando campo vazio

--- aula85/assets/js/main.js
+++ aula85/assets/js/main.js
@@ -1,14 +1,44 @@
 class ValidaFormulario {
     constructor() {
-        this.formulario = document.querySelector('formulario')
+        this.formulario = document.querySelector('.formulario');
         this.eventos();
     }
 
     eventos() {
         this.formulario.addEventListener('submit', e => {
-            this.
+            this.handleSubmit(e);
         })
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+        const camposValidos = this.camposSaoValidos();
+    }
+
+    camposSaoValidos() {
+        let valid = true;
+
+        for(let errorText of this.formulario.querySelectorAll('.error-text')) {
+            errorText.remove();
+        };
+
+        for (let campo of this.formulario.querySelectorAll('.validar')) {
+            const label = campo.previousElementSibling.innerText;
+
+            if (!campo.value) {
+                this.criaErro(campo, `Campo "${label}" não pode estar em branco`)
+                let valid = false;
+            }
+
+        };
+    }
+
+    criaErro(campo, msg) {
+        const div = document.createElement('div');
+        div.innerHTML = msg;
+        div.classList.add('error-text');
+        campo.insertAdjacentElement('afterend', div);
     }
 }
 
-const valida = ValidaFormulario();
+const valida = new ValidaFormulario();
