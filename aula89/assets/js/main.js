3c020660e4a9ac58d66922c1a68111a02afe4e15
Rafael Arcanjo <rafael.arcanjo.dssilva@gmail.com>
2025-06-19 23:46:41 -0300

    Continuação da aula sobre Ajax

--- aula89/assets/js/main.js
+++ aula89/assets/js/main.js
@@ -4,7 +4,7 @@ const request = obj => {
     xhr.send();
 
     xhr.addEventListener('load', () => {
-        if(xhr.status >= 200 && xhr.status < 300) {
+        if (xhr.status >= 200 && xhr.status < 300) {
             obj.success(xhr.responseText);
         } else {
             obj.error(xhr.statusText);
@@ -16,7 +16,7 @@ document.addEventListener('click', e => {
     const el = e.target;
     const tag = el.tagName.toLowerCase();
 
-    if(tag === 'a') {
+    if (tag === 'a') {
         e.preventDefault();
         carregaPagina(el);
     }
@@ -24,4 +24,22 @@ document.addEventListener('click', e => {
 
 function carregaPagina(el) {
     const href = el.getAttribute('href');
+
+    const objConfig = {
+        method: 'GET',
+        url: href,
+        success(response) {
+            carregaResultado(response);
+        },
+        error(errorText) {
+            console.log(errorText)
+        }
+    };
+
+    request(objConfig);
 }
+
+function carregaResultado(response) {
+    const resultado = document.querySelector('.resultado');
+    resultado.innerHTML = response;
+};
