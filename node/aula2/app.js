1c3b314978b5ac0264ff3e8b3fc1a2a55cac9644
Rafael Arcanjo <rafael.arcanjo.dssilva@gmail.com>
2025-07-07 21:15:55 -0300

    Node modules

---
+++ node/aula2/app.js
@@ -0,0 +1,17 @@
+//const Cachorro = require('./Z/mod2')
+
+//const cachorrinho = new Cachorro('Thor');
+//cachorrinho.latir()
+
+//=======================================
+
+//const path = require('path');
+//console.log(__dirname)
+//console.log(path.resolve(__dirname, '..', '..', 'arquivos', 'imagens'));
+
+//=======================================
+
+const Cachorro = require('./Z/mod2.js')
+const c1 = new Cachorro('Dog');
+c1.latir();
+
