170f292b0703466e07c04e40deb0a4a5825c70e3
Rafael Arcanjo <rafael.arcanjo.dssilva@gmail.com>
2025-07-09 23:57:48 -0300

    fs readdir

--- node/aula4/index.js
+++ node/aula4/index.js
@@ -4,13 +4,26 @@ const path = require('path');
 async function readdir(rootDir) {
     rootDir = rootDir || path.resolve(__dirname);
     const files = await fs.readdir(rootDir);
-    Walk(files);
+    Walk(files, rootDir);
 };
 
-function Walk(files) {
+async function Walk(files, rootDir) {
     for(let file of files) {
-        console.log(file)
+        const fileFullPath = path.resolve(rootDir, file)
+        const stats = await fs.stat(fileFullPath);
+
+        if (/\.git/g.test(fileFullPath)) continue;
+        if (/node_modules/g.test(fileFullPath)) continue;
+        
+        if(stats.isDirectory()) {
+            readdir(fileFullPath);
+            continue;
+        }
+
+        if (!/\.html$/g.test(fileFullPath)) continue;
+
+        console.log(fileFullPath)
     }
 }
 
-readdir()
+readdir('C:/Users/Rafael Arcanjo/Documents/Estudos/Udemy/JavaScript-TypeScript/');
